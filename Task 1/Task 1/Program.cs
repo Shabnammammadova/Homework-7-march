a18023994e8a1bf9093ab4dbe0e5fb7fc320b737
Şəbnəm <mmmdovasbnm47@gmail.com>
2023-03-08 19:03:03 +0400

    first commit

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Task 1
+int num = 14;
+bool result = false;
+for (int i = 2; i < num; i++) ;
+{
+
+
+    if (num % 2== 0) ;
+    {
+        result = true;
+       
+    }
+}
+if (result)
+{
+    Console.WriteLine("Murekkebdir");
+}
+
+else
+{
+
+    Console.WriteLine("sadedir");
+}
+
+
+
